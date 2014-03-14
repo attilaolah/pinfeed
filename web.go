9a6003be171cfdde8b2b16db211d400afed5a641
Attila Oláh <attilaolah@gmail.com>
2014-03-14 16:54:57 +0700

    working proxy for pinterest feeds

--- web.go
+++ web.go
@@ -1,21 +1,44 @@
 package main
 
 import (
-    "fmt"
-    "net/http"
-    "os"
+    "io"
+	"net/http"
+	"os"
+    "strings"
 )
 
 func main() {
-    http.HandleFunc("/", hello)
-    fmt.Println("listening...")
-    err := http.ListenAndServe(":"+os.Getenv("PORT"), nil)
+	http.HandleFunc("/", pinfeed)
+	if err := http.ListenAndServe(":"+os.Getenv("PORT"), nil); err != nil {
+		panic(err)
+	}
+}
+
+func pinfeed(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path == "/" {
+        // TODO: some home page would be nice…
+		w.WriteHeader(http.StatusNoContent)
+		return
+	}
+    res, err := http.Get(feedURL(username(r.URL.Path)))
     if err != nil {
-      panic(err)
+		w.WriteHeader(http.StatusInternalServerError)
+        return
     }
+    defer res.Body.Close()
+    for key, vals := range res.Header {
+        for _, val := range vals {
+            w.Header().Add(key, val)
+        }
+    }
+    w.WriteHeader(res.StatusCode)
+    io.Copy(w, res.Body)
 }
 
-func hello(res http.ResponseWriter, req *http.Request) {
-    fmt.Fprintln(res, "hello, world")
+func feedURL(username string) string {
+	return "https://www.pinterest.com/" + username + "/feed.rss"
 }
 
+func username(path string) string {
+    return strings.SplitN(path, "/", 3)[1]
+}
