a682a64801da81b9ad252d43ddd642535fb10445
Attila Oláh <attilaolah@gmail.com>
2014-03-15 21:24:45 +0700

    replacement working now

--- web.go
+++ web.go
@@ -1,60 +1,65 @@
 package main
 
 import (
-    "io"
-    "io/ioutil"
+	"io"
+	"io/ioutil"
 	"net/http"
 	"os"
-    "regexp"
-    "strings"
+	"regexp"
+	"strings"
 )
 
-var thumb = regexp.MustCompile("https?://media-cache-[0-9a-z]+.pinimg.com/192x/[0-9a-f]{2}/[0-9a-f]{2}/[0-9a-f]{2}/[0-9a-f]{32}.jpg")
+const origin = "https://www.pinterest.com/"
+
+var (
+	thumb       = regexp.MustCompile("\\b(https?://[0-9a-z-]+.pinimg.com/)192(x/[/0-9a-f]+.jpg)\\b")
+	replacement = []byte("${1}736${2}")
+)
 
 func main() {
-	http.HandleFunc("/", pinfeed)
+	http.HandleFunc("/", pinFeed)
 	if err := http.ListenAndServe(":"+os.Getenv("PORT"), nil); err != nil {
 		panic(err)
 	}
 }
 
-func pinfeed(w http.ResponseWriter, r *http.Request) {
+func pinFeed(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path == "/" {
-        // TODO: some home page would be nice…
+		// TODO: some home page would be nice…
 		w.WriteHeader(http.StatusNoContent)
 		return
 	}
-    res, err := http.Get(feedURL(username(r.URL.Path)))
-    if err != nil {
+	res, err := http.Get(feedURL(username(r.URL.Path)))
+	if err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
-        return
-    }
-    defer res.Body.Close()
-    for key, vals := range res.Header {
-        for _, val := range vals {
-            w.Header().Add(key, val)
-        }
-    }
-    w.WriteHeader(res.StatusCode)
-    buf, err := replaceAllThumbs(res.Body)
-    if err != nil {
+		return
+	}
+	defer res.Body.Close()
+	for key, vals := range res.Header {
+		for _, val := range vals {
+			w.Header().Add(key, val)
+		}
+	}
+	w.WriteHeader(res.StatusCode)
+	buf, err := replaceThumbs(res.Body)
+	if err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
-        return
-    }
-    w.Write(buf)
+		return
+	}
+	w.Write(buf)
 }
 
 func feedURL(username string) string {
-	return "https://www.pinterest.com/" + username + "/feed.rss"
+	return origin + "/" + username + "/feed.rss"
 }
 
 func username(path string) string {
-    return strings.SplitN(path, "/", 3)[1]
+	return strings.SplitN(path, "/", 3)[1]
 }
 
-func replaceAllThumbs(r io.Reader) (buf []byte, err error) {
-    if buf, err = ioutil.ReadAll(r); err != nil {
-        return
-    }
-    return
+func replaceThumbs(r io.Reader) (buf []byte, err error) {
+	if buf, err = ioutil.ReadAll(r); err == nil {
+		buf = thumb.ReplaceAll(buf, replacement)
+	}
+	return
 }
