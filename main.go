da5fa4091382478bb48a47c53fc31996cfa7480f
Attila Oláh <attilaolah@google.com>
2015-12-08 09:02:52 +0100

    temporarily disable compression

--- main.go
+++ main.go
@@ -7,6 +7,8 @@ import (
 )
 
 func main() {
-	http.HandleFunc("/", compress(pinFeed))
+	// Disable compression until we figure out what's wrong.
+	//http.HandleFunc("/", compress(pinFeed))
+	http.HandleFunc("/", pinFeed)
 	log.Fatal(http.ListenAndServe(":"+os.Getenv("PORT"), nil))
 }
